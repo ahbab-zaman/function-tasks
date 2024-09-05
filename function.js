dac5c02ac19d976a56b87a7691dac202cdfdc9af
Ahbabuzzaman <160356472+ahbab-zaman@users.noreply.github.com>
2024-09-06 02:17:31 +0600

    second commit

---
+++ function.js
@@ -0,0 +1,89 @@
+// Task 1
+
+// Take four parameters. Multiply the four numbers and then return the result
+
+// function multiplyNumbers(a, b, c, d){
+//     const result = a * b * c * d;
+//     return result;
+// }
+// const result = multiplyNumbers(2, 3, 4, 5);
+// const result2 = multiplyNumbers(1, 2, 3, 4);
+
+// console.log(result);
+// console.log(result2)
+
+// Task 2
+
+// Take a number if the number is odd multiply it by 2 and return the result. If the number is even divide it by two and return the result.
+
+// function evenOdd(number){
+//         if (number % 2 === 1) {
+//             const result = number * 2;
+//             return result;
+//         } else{
+//            const result = number / 2;
+//             return result;
+//         }
+// }
+// const result = evenOdd(101);
+// console.log(result);
+
+// Task 3
+
+// Write a function called make_avg() which will take an array of integers and the size of that array and return the average of those values.
+
+// function make_avg(numbers){
+//     let sum = 0;
+//      for(i = 0; i < numbers.length; i++){
+//         sum += numbers[i];
+//      }
+//         return sum / numbers.length;
+// }
+// let numbers = [2, 3, 5, 6, 7];
+// let size = numbers.length;
+// console.log(make_avg(numbers));
+// console.log(size);
+
+// Task 4
+
+// Write a function called count_zero() which will take a binary string (Binary string is a string which is consist of only 0 and 1) as parameter and count how many 0’s are there in that string.
+
+// function count_zero(str){
+//    let findZero = 0;
+//    for(let i = 0; i < str.length; i++){
+//     if (str[i] == '0') {
+//     findZero++;        
+//     }
+//    }
+//    return findZero;
+// }
+// const findZero = count_zero('00100110');
+// console.log(findZero);
+// console.log('oiafuhgaesifhgdsiufchgdsf');
+// console.log(count_zero('000000010001'));
+
+// Task 5
+
+// Write a function called odd_even() which takes an integer value and tells whether this value is even or odd. If even return Even. If odd return Odd
+
+// function odd_even(number){
+//     if (number % 2 === 0) {
+//         return 'even';
+//     } else {
+//         return 'Odd';
+//     }
+// } 
+// console.log(odd_even(22));
+
+
+// console.log('aifuahgdsifhgafidasufh');
+
+// console.log(odd_even(40));
+// console.log(odd_even(111));
+// console.log(odd_even(103));
+// console.log(odd_even(44));
+// console.log(odd_even(79));
+// console.log(odd_even(98));
+// console.log(odd_even(100));
+// console.log(odd_even(2221));
+// console.log(odd_even(0));
